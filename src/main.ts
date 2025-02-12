27fd689443605300e08a5523fd07cc69317891da
@a.malkov_automacon.ru <a.malkov@automacon.ru>
2025-02-12 18:56:50 +0700

    fix session import

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 import RedisStore from 'connect-redis'
 import cookieParser from 'cookie-parser'
-import * as session from 'express-session'
+import session from 'express-session'
 import IORedis from 'ioredis'
 
 import { AppModule } from './app.module'
